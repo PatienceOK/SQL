3d76b076fc925e10551e2f1eb6cfd785984fbaca
PatienceOK <125620431+PatienceOK@users.noreply.github.com>
2023-03-05 01:06:55 -0600

    Update COVID Portfolio Project.sql

--- COVID Portfolio Project.sql
+++ COVID Portfolio Project.sql
@@ -1,4 +1,6 @@
 
+--- Covid 19 Data Exploration 
+--- Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 ---Select Data that we are going to be starting with
 
